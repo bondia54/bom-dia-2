046e752c80df7020b6c2c5aacea7cb341ca76352
Heitor15672 <137784864+Heitor15672@users.noreply.github.com>
2025-05-09 09:49:27 -0300

    GG

--- 2ºTRI ++/Alex do Durex/Front End/desvioCondicional/js/script.js
+++ 2ºTRI ++/Alex do Durex/Front End/desvioCondicional/js/script.js
@@ -21,14 +21,22 @@ function Triangulo(){
 }
 
 function Triângulo(){
-    LadoX = parseInt(document.getElementById('X').value);
-    LadoY = parseInt(document.getElementById('Y').value);
-    LadoZ = parseInt(document.getElementById('Z').value);
+    x = parseInt(document.getElementById('X').value);
+    y = parseInt(document.getElementById('Y').value);
+    z = parseInt(document.getElementById('Z').value);
     resposta = "";
 
-    if(LadoX == LadoY == LadoZ){
-        resposta = "Triângulo Equilátero"
-    }else if(LadoX == )
+    if (x < y + z && y < x + z && z < x + y) {
+        if (x == y && y == z) {
+          resposta = "É um triângulo equilátero, seu animal!";
+        } else if (x == y || x == z || y == z) {
+            resposta= "É um triângulo isósceles, seu burro!";
+        } else {
+            resposta= "É um triângulo escaleno, seu idiota!";
+        }
+      } else {
+        resposta= "Essa porcaria não forma um triângulo, seu retardado!";
+      }
 
     document.getElementById("resposta").innerHTML = resposta;
 }
