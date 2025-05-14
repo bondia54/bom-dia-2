902f0335f2e0cd7e09070ae4afceb8ed511c808b
Heitor15672 <137784864+Heitor15672@users.noreply.github.com>
2025-05-14 10:11:57 -0300

    python2

--- 2ºTRI ++/Julião Botijão/python/PrimeiraVez/app.py
+++ 2ºTRI ++/Julião Botijão/python/PrimeiraVez/app.py
@@ -6,6 +6,11 @@ nome = input("digite seu nome: ") #input string
 #variavel calculando a média
 media = (nota1 + nota2) / 2
 
-#formas de mostrar sua string junto da variavel
+#formas de mostrar sua string junto da variavel:
 #print("sua nota é", media)
-print(f"{nome}, sua média é {media}")
+#print(f"{nome}, sua média é {media}")
+
+if media < 6:
+    print(f"mano, tu tirou {media}, {nome}, seu otário")
+else:
+    print(f"parabens, {nome}, tu passou com {media}")
