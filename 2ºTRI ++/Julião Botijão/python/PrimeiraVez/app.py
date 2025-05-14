ec0cbeffcb70d5fa6ce75ea93dbb07192117fb63
Heitor15672 <137784864+Heitor15672@users.noreply.github.com>
2025-05-14 10:01:13 -0300

    Python do Julião

---
+++ 2ºTRI ++/Julião Botijão/python/PrimeiraVez/app.py
@@ -0,0 +1,11 @@
+#criando variáveis
+nota1 = float(input("digite sua nota 1: ")) #input numérico
+nota2 = float(input("digite sua nota 2: ")) #input numérico
+nome = input("digite seu nome: ") #input string
+
+#variavel calculando a média
+media = (nota1 + nota2) / 2
+
+#formas de mostrar sua string junto da variavel
+#print("sua nota é", media)
+print(f"{nome}, sua média é {media}")
