229ac194f1087b0b284a738b82bcc84f542ea780
Heitor15672 <137784864+Heitor15672@users.noreply.github.com>
2025-05-16 12:19:15 -0300

    py

--- 2ºTRI ++/Julião Botijão/python/PrimeiraVez/app.py
+++ 2ºTRI ++/Julião Botijão/python/PrimeiraVez/app.py
@@ -1,16 +1,25 @@
-#criando variáveis
-nota1 = float(input("digite sua nota 1: ")) #input numérico
-nota2 = float(input("digite sua nota 2: ")) #input numérico
-nome = input("digite seu nome: ") #input string
 
-#variavel calculando a média
-media = (nota1 + nota2) / 2
+while True:
 
-#formas de mostrar sua string junto da variavel:
-#print("sua nota é", media)
-#print(f"{nome}, sua média é {media}")
+    #criando variáveis
+    nome = input("digite seu nome: ") #input string
+    nota1 = float(input("digite sua primeira nota: ")) #input numérico
+    nota2 = float(input("digite sua segunda nota: ")) #input numérico
 
-if media < 6:
-    print(f"mano, tu tirou {media}, {nome}, seu otário")
-else:
-    print(f"parabens, {nome}, tu passou com {media}")
+    #variavel calculando a média
+    media = (nota1 + nota2) / 2
+
+    #formas de mostrar sua string junto da variavel:
+    #print(nome", sua nota é", media)
+    #print(f"{nome}, sua média é {media}")
+
+    if media < 6:
+        print(f"mano, tu tirou {media}, {nome}, seu otário")
+    else:
+        print(f"parabens, {nome}, tu passou com {media}")
+
+    continuar = input("deseja continuar novamente? [s/n] ")
+
+    if continuar != "s":
+        print("saindo...")
+        break
