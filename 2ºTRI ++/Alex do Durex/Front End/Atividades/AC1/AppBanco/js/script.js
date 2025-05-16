7021fe039aa2b30e9e6faf3d32bac1125550c06b
Heitor15672 <137784864+Heitor15672@users.noreply.github.com>
2025-05-16 09:30:03 -0300

    ok

---
+++ 2ºTRI ++/Alex do Durex/Front End/Atividades/AC1/AppBanco/js/script.js
@@ -0,0 +1,25 @@
+function calcular(){
+    saldo = parseFloat(document.getElementById('Saldo').value);
+    emprestimo = 0;
+    resposta = "";
+
+    if (saldo < 0) {
+        resposta = "Saldo negativo. Digite um valor positivo.";
+    }
+    else {
+        if(saldo <= 1300){
+            emprestimo = saldo + (0.2 * saldo);
+        } else if (saldo <= 3000){
+            emprestimo = saldo + (0.3 * saldo);
+        } else if (saldo <= 8000){
+            emprestimo = saldo + (0.45 * saldo);
+        } else if (saldo <= 10000){
+            emprestimo = saldo + (0.5 * saldo);
+        } else{
+            emprestimo = saldo + (0.6 * saldo);
+        }
+        resposta = `Seu empréstimo é de ${emprestimo.toFixed(2)}`;
+    }
+   
+    document.getElementById("resposta").innerHTML = resposta;
+}
